853ced507a057520e5d7cb46379f29c6ee50b74f
Glitch (periodic-delightful-yellowhorn) <none>
2021-03-31 17:52:35 +0000

    🌞💇 Checkpoint ./public/js/client.js:6827812/151

--- public/js/client.js
+++ public/js/client.js
@@ -130,11 +130,7 @@ var Client = (function(window) {
     
     if(selected==null){
       //first find the board
-      for(let i = 0; i < n; i++){
-        for(let j = 0; j < n; j++){
-          
-        }
-      }
+      for(let tli in gameState.spacetime)
       selected = {};
     }
   });
