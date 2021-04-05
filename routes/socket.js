82fd66a4b418ac38f96fb8a5a9ccf61fa8724814
Glitch (periodic-delightful-yellowhorn) <none>
2021-04-05 05:36:55 +0000

    🚖📺 Checkpoint ./routes/socket.js:6827812/1284

--- routes/socket.js
+++ routes/socket.js
@@ -1,6 +1,33 @@
 var IO = null;
 var DB = null;
 
+function deepClone(obj, hash = new WeakMap()) {
+  // Do not try to clone primitives or functions
+  if (Object(obj) !== obj || obj instanceof Function) return obj;
+  if (hash.has(obj)) return hash.get(obj); // Cyclic reference
+  try {
+    // Try to run constructor (without arguments, as we don't know them)
+    var result = new obj.constructor();
+  } catch (e) {
+    // Constructor failed, create object without running the constructor
+    result = Object.create(Object.getPrototypeOf(obj));
+  }
+  // Optional: support for some standard constructors (extend as desired)
+  if (obj instanceof Map)
+    Array.from(obj, ([key, val]) =>
+      result.set(deepClone(key, hash), deepClone(val, hash))
+    );
+  else if (obj instanceof Set)
+    Array.from(obj, key => result.add(deepClone(key, hash)));
+  // Register in hash
+  hash.set(obj, result);
+  // Clone and assign enumerable own properties recursively
+  return Object.assign(
+    result,
+    ...Object.keys(obj).map(key => ({ [key]: deepClone(obj[key], hash) }))
+  );
+}
+
 /**
  * Add player to game
  * Emits an "update" event on success or an "error" event on failure
@@ -92,6 +119,15 @@ var move = function(data) {
 
   console.log(data.gameID+' '+sess.playerName+': '+data.move);
 };
+
+var validcalc = function(data){
+  const temp = deepCopygame.spacetime;
+  
+  
+  game.getMoves();
+  IO.sockets.in(data.gameID).emit('recalc', game);
+  game.spacetime = temp;
+}
 
 /**
  * Forfeit a game
